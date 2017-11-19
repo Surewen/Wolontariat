98170379d28f445bd622c938913bb4a8bedbb928
Michal Saniuk <m.saniuk@wp.pl>
2017-11-20 00:02:42 +0100

    Dodanie obsługi informacji o Cookies.

--- Wolontariat/Template.Master.cs
+++ Wolontariat/Template.Master.cs
@@ -11,7 +11,27 @@ namespace Wolontariat
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //CHECK COOKIES CONSENT
+            if (Request.Cookies["CookieConsent"] != null)
+            {
+                cookieConsent.Visible = false;
+            }
+        }
 
+
+
+
+
+
+
+
+        protected void Accept_Cookies(object sender, EventArgs e)
+        {
+            HttpCookie cookie_consent = new HttpCookie("CookieConsent");
+            cookie_consent.Value = "yes";
+            cookie_consent.Expires = DateTime.Now.AddMinutes(5);
+            Response.Cookies.Add(cookie_consent);
+            cookieConsent.Visible = false;
         }
     }
 }
--- Wolontariat/Template.Master.designer.cs
+++ Wolontariat/Template.Master.designer.cs
@@ -29,5 +29,23 @@ namespace Wolontariat {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// cookieConsent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl cookieConsent;
+        
+        /// <summary>
+        /// consent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button consent;
     }
 }
