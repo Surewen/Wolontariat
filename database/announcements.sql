3d6c76377650785d949c378285ad27bfd0a540cf
discocommando123 <33530330+discocommando123@users.noreply.github.com>
2017-11-30 12:58:43 +0100

    announcements update
    Aktualizacja danych w announcements

---
+++ database/announcements.sql
@@ -0,0 +1,13 @@
+INSERT INTO announcements(post_date, created_by, current_status, type, content) VALUES
+(CONVERT(DATETIME, '2017-01-25', 102), 'Birdie','w trakcie', 'volunteer', 'Pomoc doraźna, pomoc ludziom starszym'),
+(CONVERT(DATETIME, '2016-07-14', 102), 'Lolek','w trakcie', 'needy', 'Chora córka po wypadku. Złamane wszystkie żebra'),
+(CONVERT(DATETIME, '2017-03-01', 102), 'Micha1','w trakcie', 'needy', 'Niedowład nóg, przekuplina oraz uraz psychiczny'),
+(CONVERT(DATETIME, '2014-05-29', 102), 'Sztazi','zakończony', 'needy', 'Złamane obie nogi, spadł z drabiny'),
+(CONVERT(DATETIME, '2017-09-25', 102), 'Rzolniez18','w trakcie', 'volunteer', 'Co mogę to pomogę'),
+(CONVERT(DATETIME, '2014-11-24', 102), 'LubiePiwo','zakończone', 'volunteer', 'Pomoc w robieniu zakupów i wyprowadzaniu psa'),
+(CONVERT(DATETIME, '2017-01-11', 102), 'Luksusowa40','w trakcie', 'needy', 'Osoba leżąca, niedowład nóg po wypadku samochodowym'),
+(CONVERT(DATETIME, '2015-12-25', 102), 'Tyskie24','w trakcie', 'needy', 'Osoba niepełnosprawna jeżdżąca na wózku'),
+(CONVERT(DATETIME, '2017-06-01', 102), 'ZielonyBeret','zakończony', 'volunteer', 'Rozmowa z osobami starszymi, odwiedzanie, pomoc psychologiczna'),
+(CONVERT(DATETIME, '2016-07-25', 102), 'MalyKomandos','zakończony', 'needy', 'Skręcona kostka, pomoc w robieniu zakupów'),
+(CONVERT(DATETIME, '2017-03-12', 102), 'Damian34','w trakcie', 'needy', 'Nietrzymanie moczu, pomoc w utrzymaniu higieny osobistej'),
+(CONVERT(DATETIME, '2015-01-21', 102), 'Dziku','w trakcie', 'volunteer', 'Organizacja zbiórek pieniędzy, imprez charytatywnych');
