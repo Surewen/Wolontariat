f3685745d1d108178a52e070f749538f8a4d0781
Łukasz Maszkiewicz <maszkiewiczlukasz@gmail.com>
2017-11-30 10:53:45 +0100

    Revert "cxcxzx"
    This reverts commit e80c3ecd4c0c6a0490f91eea37d1eee7cf5ad058.

--- Wolontariat/Announcements.aspx.cs
+++ Wolontariat/Announcements.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Wolontariat
 {
-    public partial class AddAnnouncement : System.Web.UI.Page
+    public partial class Announcements : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
--- Wolontariat/Register.aspx.cs
+++ Wolontariat/Register.aspx.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,18 +9,9 @@ namespace Wolontariat
 {
     public partial class Register : System.Web.UI.Page
     {
-        protected void RegisterUser(object sender, EventArgs e)
+        protected void Page_Load(object sender, EventArgs e)
         {
-            try
-            {
-                SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["pww-databaseConnectionString"].ConnectionString);
-                con.Open();
-                string insert = "insert into users (pesel, email, name, surname, birth_date, sex, type) values (@pesel, @email, @name, @surname, @birth_date, @sex, @type) ";
-                SqlCommand cmd = new SqlCommand(insert, con);
-               
-            }
-            catch (Exception ef)
-            { }
+
         }
     }
 }
