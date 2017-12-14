b8ee82b79f6cdc7460cc4879b4712a9d41e6d94f
Michal Saniuk <m.saniuk@wp.pl>
2017-12-14 16:47:34 +0100

    Dodanie komentarzy do EditEvent

--- Wolontariat/EditEvent.aspx.cs
+++ Wolontariat/EditEvent.aspx.cs
@@ -7,13 +7,26 @@ using System.Web.UI.WebControls;
 
 namespace Wolontariat
 {
+    /// <summary>
+    /// This class is a partial class responsible for editing events.
+    /// </summary>
     public partial class EditEvent : System.Web.UI.Page
     {
         SQLDatabase db;
+        /// <summary>
+        /// This method executes when the page loasds but is empty since everything needed for editing loads from aspx file.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+        /// <summary>
+        /// This method is responsible for making a connection to database, trying to put the edited data into the database and returning to the events list.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void Edit_Event(object sender, EventArgs e)
         {
             db = new SQLDatabase();
